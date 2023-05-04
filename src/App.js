47210e0b4cc44bf672cb78a3e43078878485f976
Danny Hobart <dh@dannyhobart.com>
2023-05-04 16:57:58 -0400

    Kind of mucked - big commit - need to branch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import * as React from 'react'
+
+function Element({value}){
+  const title = value.title;
+  const description = value.description;
+  console.log("element initiated");
+  return (
+    <div className="Element">
+      <div className="El-header">
+        <p>{title}</p>
+      </div>
+      <div className="El-content">
+        <p>{description}</p>
+      </div>
+    </div>
+  )
+}
+
+function UserInput({count, onCountChange, createDisplay}){
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <label htmlFor="count">How many: </label>
+        <input value={count} onChange={onCountChange} id="count" />
+      </form>
+      <button value={count} onClick={createDisplay}>
+        Create Display
+      </button>
+    </div>
+  )
+}
+/*
+function Accordion({elements}){
+
+  console.log(...elements);
+  return <div>{`This many: ${elements.length}`}</div>
+
+}
+*/
+function App({initialCount = 1}) {
+  const [count, setCount] = React.useState(
+    window.localStorage.getItem('count') ?? initialCount
+  )
+  var elements = [];
+  var tempObj = {title:"Here's the title", description:"this is the content"};
+  var tempArray = new Array(tempObj, tempObj, tempObj, tempObj, tempObj);
+
+  // const [elements, setElements] = React.useState(
+  //   window.localStorage.getItem('elements') ?? []
+  // )
+
+  React.useEffect(() => {
+    window.localStorage.setItem('count', count);
+    //window.localStorage.setItem('elements', elements);
+  })
+
+  function createDisplay() {
+    var dataArray = new Array(tempArray.slice(0, count));
+    console.log(dataArray);
+
+    elements = dataArray.map( item => <Element value={ item } />);
+    console.log(elements);
+  }
+
+  return (
+    <div className="App-header">
+      <UserInput
+        count={count}
+        onCountChange={event => setCount(event.target.value)}
+        createDisplay={createDisplay}
+      />
+      {elements}
     </div>
   );
 }

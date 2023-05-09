098bfdbafa913774853dd66e928d011b1f776d5f
Danny Hobart <dh@dannyhobart.com>
2023-05-09 16:03:17 -0400

    set up fetch call
    this is not fully working. fetch call is, I just need to solve for distributing the actual call to the elements

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import {grabData} from './fetchdata.js'
 
 import * as React from 'react'
 
 function Element({value}){
+  console.log('from element')
+  console.log(value)
   const [isActive, setActive] = React.useState(false);
-  const title = value.title;
-  const description = value.description;
+  const title = "hello"//value.title;
+  const description = "world"//value.body;
   return (
     <div className="Element" onClick={
         () => {
@@ -24,30 +27,41 @@ function Element({value}){
 }
 
 function UserInput({count, onCountChange, createDisplay}){
+  const [canSubmit, setCanSubmit] = React.useState(false);
+
+  function onButtonClick(){
+
+  }
+  function onValueChange(){
+    console.log('value changed')
+    console.log(count)
+  }
 
   return (
     <div className="App">
       <form>
         <label htmlFor="count">How many: </label>
-        <input value={count} onChange={onCountChange} id="count" />
+        <input value={count} onChange={onValueChange} id="count" />
       </form>
-      <button value={count} onClick={createDisplay}>
+      <button disabled={canSubmit} value={count} onClick={onButtonClick}>
         Create Display
       </button>
     </div>
   )
 }
 function App({initialCount = 1}) {
-  const [count, setCount] = React.useState(
-    window.localStorage.getItem('count') ?? initialCount
-  )
 
+  const [count, setCount] = React.useState(initialCount)
+
+  //var dataArray = [];
   var dataArray = [tempObj,tempObj,tempObj,tempObj,tempObj,tempObj,tempObj];
 
   var tempObj = {title:"Here's the title", description:"this is the content"};
   var tempArray = new Array(tempObj, tempObj, tempObj, tempObj, tempObj);
 
   var elements = [];
+
+
 
   // const [elements, setElements] = React.useState(
   //   window.localStorage.getItem('elements') ?? []
@@ -57,13 +71,20 @@ function App({initialCount = 1}) {
     window.localStorage.setItem('count', count);
     //window.localStorage.setItem('elements', elements);
   })
+  ;
 
   function createDisplay() {
-    dataArray = tempArray.slice(0, count);
+    //dataArray = tempArray.slice(0, count);
+    dataArray = grabData('https://jsonplaceholder.typicode.com/posts', 3).then(
+      console.log('promise returned'),
+      console.log(dataArray),
+      elements = dataArray.map( (item, index) => {
+        console.log('building elements')
+        return (<Element key={ index } value={ item } />)
+      }),
+    )
 
-    elements = dataArray.map( (item, index) => {
-      return (<Element key={ index } value={ item } />)
-    });
+
   }
 
   createDisplay();
---
+++ src/fetchdata.js
@@ -0,0 +1,19 @@
+import * as React from 'react'
+
+//https://jsonplaceholder.typicode.com/posts?_limit=10
+
+function grabData(base, limit){
+  const url = base + "?_limit=" + limit;
+
+  return window.fetch(url)
+  .then((response) => response.json())
+  .then((json) => {
+    console.log(json)
+    return (json)
+  });
+}
+
+
+export {
+  grabData,
+}
